986bee15eb6d361d12e5eccbd80af0c4c1d53bfc
Anderson Farias <andersonsantofarias@outlook.com>
2022-07-16 15:01:19 -0300

    feat: Paginação com PagingAndSortingRepository

--- src/main/java/br/com/alura/spring/data/repository/FuncionarioRepository.java
+++ src/main/java/br/com/alura/spring/data/repository/FuncionarioRepository.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Repository;
 import br.com.alura.spring.data.orm.Funcionario;
 
 @Repository
-public interface FuncionarioRepository extends CrudRepository<Funcionario, Integer> {
+public interface FuncionarioRepository extends PagingAndSortingRepository<Funcionario, Integer> {
 	List<Funcionario> findByNome(String nome);
 	
 	List<Funcionario> findByNomeAndSalarioGreaterThanAndDataContratacao(String nome, Double salario, LocalDate data);
--- src/main/java/br/com/alura/spring/data/service/CrudFuncionarioService.java
+++ src/main/java/br/com/alura/spring/data/service/CrudFuncionarioService.java
@@ -25,31 +25,29 @@ public class CrudFuncionarioService {
 
 	private Boolean system = true;
 	private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-	
+
 	private final CargoRepository cargoRepository;
 	private final FuncionarioRepository funcionarioRepository;
 	private final UnidadeTrabalhoRepository unidadeTrabalhoRepository;
-	
-	
-	public CrudFuncionarioService(FuncionarioRepository funcionarioRepository, 
-			CargoRepository cargoRepository, 
+
+	public CrudFuncionarioService(FuncionarioRepository funcionarioRepository, CargoRepository cargoRepository,
 			UnidadeTrabalhoRepository unidadeTrabalhoRepository) {
 		this.cargoRepository = cargoRepository;
 		this.funcionarioRepository = funcionarioRepository;
 		this.unidadeTrabalhoRepository = unidadeTrabalhoRepository;
 	}
-	
+
 	public void inicial(Scanner scanner) {
-		while(system) {
+		while (system) {
 			System.out.println("Qual acao de funcionario deseja executar");
 			System.out.println("0 - Sair");
 			System.out.println("1 - Salvar");
 			System.out.println("2 - Atualizar");
 			System.out.println("3 - Visualizar");
 			System.out.println("4 - Deletar");
-			
+
 			int action = scanner.nextInt();
-			
+
 			switch (action) {
 			case 1:
 				salvar(scanner);
@@ -67,103 +65,109 @@ public class CrudFuncionarioService {
 				system = false;
 				break;
 			}
-			
 		}
-		
 	}
-	
+
 	private void salvar(Scanner scanner) {
 		System.out.println("Digite o nome");
-        String nome = scanner.next();
+		String nome = scanner.next();
 
-        System.out.println("Digite o cpf");
-        String cpf = scanner.next();
+		System.out.println("Digite o cpf");
+		String cpf = scanner.next();
 
-        System.out.println("Digite o salario");
-        Double salario = scanner.nextDouble();
+		System.out.println("Digite o salario");
+		Double salario = scanner.nextDouble();
 
-        System.out.println("Digite a data de contracao");
-        String dataContratacao = scanner.next();
+		System.out.println("Digite a data de contracao");
+		String dataContratacao = scanner.next();
 
-        System.out.println("Digite o cargoId");
-        Integer cargoId = scanner.nextInt();
+		System.out.println("Digite o cargoId");
+		Integer cargoId = scanner.nextInt();
 
-        List<UnidadeTrabalho> unidades = unidade(scanner);
+		List<UnidadeTrabalho> unidades = unidade(scanner);
 
-        Funcionario funcionario = new Funcionario();
-        funcionario.setNome(nome);
-        funcionario.setCpf(cpf);
-        funcionario.setSalario(salario);
-        funcionario.setDataContratacao(LocalDate.parse(dataContratacao, formatter));
-        Optional<Cargo> cargo = cargoRepository.findById(cargoId);
-        funcionario.setCargo(cargo.get());
-        funcionario.setUnidadeTrabalhos(unidades);
+		Funcionario funcionario = new Funcionario();
+		funcionario.setNome(nome);
+		funcionario.setCpf(cpf);
+		funcionario.setSalario(salario);
+		funcionario.setDataContratacao(LocalDate.parse(dataContratacao, formatter));
+		Optional<Cargo> cargo = cargoRepository.findById(cargoId);
+		funcionario.setCargo(cargo.get());
+		funcionario.setUnidadeTrabalhos(unidades);
 
-        funcionarioRepository.save(funcionario);
-        System.out.println("Salvo");
+		funcionarioRepository.save(funcionario);
+		System.out.println("Salvo");
 	}
-	
+
 	private List<UnidadeTrabalho> unidade(Scanner scanner) {
-        Boolean isTrue = true;
-        List<UnidadeTrabalho> unidades = new ArrayList<>();
+		Boolean isTrue = true;
+		List<UnidadeTrabalho> unidades = new ArrayList<>();
 
-        while (isTrue) {
-            System.out.println("Digite o unidadeId (Para sair digite 0)");
-            Integer unidadeId = scanner.nextInt();
+		while (isTrue) {
+			System.out.println("Digite o unidadeId (Para sair digite 0)");
+			Integer unidadeId = scanner.nextInt();
 
-            if(unidadeId != 0) {
-                Optional<UnidadeTrabalho> unidade = unidadeTrabalhoRepository.findById(unidadeId);
-                unidades.add(unidade.get());
-            } else {
-                isTrue = false;
-            }
-        }
+			if (unidadeId != 0) {
+				Optional<UnidadeTrabalho> unidade = unidadeTrabalhoRepository.findById(unidadeId);
+				unidades.add(unidade.get());
+			} else {
+				isTrue = false;
+			}
+		}
 
-        return unidades;
-    }
-	
+		return unidades;
+	}
+
 	private void atualizar(Scanner scanner) {
 		System.out.println("Digite o id");
-        Integer id = scanner.nextInt();
+		Integer id = scanner.nextInt();
 
-        System.out.println("Digite o nome");
-        String nome = scanner.next();
+		System.out.println("Digite o nome");
+		String nome = scanner.next();
 
-        System.out.println("Digite o cpf");
-        String cpf = scanner.next();
+		System.out.println("Digite o cpf");
+		String cpf = scanner.next();
 
-        System.out.println("Digite o salario");
-        Double salario = scanner.nextDouble();
+		System.out.println("Digite o salario");
+		Double salario = scanner.nextDouble();
 
-        System.out.println("Digite a data de contracao");
-        String dataContratacao = scanner.next();
+		System.out.println("Digite a data de contracao");
+		String dataContratacao = scanner.next();
 
-        System.out.println("Digite o cargoId");
-        Integer cargoId = scanner.nextInt();
+		System.out.println("Digite o cargoId");
+		Integer cargoId = scanner.nextInt();
 
-        Funcionario funcionario = new Funcionario();
-        funcionario.setId(id);
-        funcionario.setNome(nome);
-        funcionario.setCpf(cpf);
-        funcionario.setSalario(salario);
-        funcionario.setDataContratacao(LocalDate.parse(dataContratacao, formatter));
-        Optional<Cargo> cargo = cargoRepository.findById(cargoId);
-        funcionario.setCargo(cargo.get());
+		Funcionario funcionario = new Funcionario();
+		funcionario.setId(id);
+		funcionario.setNome(nome);
+		funcionario.setCpf(cpf);
+		funcionario.setSalario(salario);
+		funcionario.setDataContratacao(LocalDate.parse(dataContratacao, formatter));
+		Optional<Cargo> cargo = cargoRepository.findById(cargoId);
+		funcionario.setCargo(cargo.get());
 
-        funcionarioRepository.save(funcionario);
-        System.out.println("Alterado");
+		funcionarioRepository.save(funcionario);
+		System.out.println("Alterado");
 	}
-	
-	private void visualizar(Scanner scanner) {		
-		Iterable<Funcionario> funcionarios = funcionarioRepository.findAll();
+
+	private void visualizar(Scanner scanner) {
+		System.out.println("Qual página vc deseja visializar?");
+		Integer pagina = scanner.nextInt();
+		
+		Pageable pegeable = PageRequest.of(pagina, 5, Sort.unsorted());
+		
+		Page<Funcionario> funcionarios = funcionarioRepository.findAll(pegeable);
+		System.out.println(funcionarios);
+		System.out.println("Página atual: " + funcionarios.getNumber());
+		System.out.println("Quantidade total: " + funcionarios.getTotalElements());
 		funcionarios.forEach(funcionario -> System.out.println(funcionario));
 	}
-	
+
 	private void deletar(Scanner scanner) {
 		System.out.println("Id");
 		int id = scanner.nextInt();
 		funcionarioRepository.deleteById(id);
 		System.out.println("Deletado");
 	}
-	
+
 }
